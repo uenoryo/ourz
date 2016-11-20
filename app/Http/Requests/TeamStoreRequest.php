6b1d8789ad849297cb33127fda992720d9cb792c
uenoryo <r.r.ag0611@outlook.jp>
2016-11-20 18:41:18 +0900

    teamsのバリデーションを設定

--- app/Http/Requests/TeamStoreRequest.php
+++ app/Http/Requests/TeamStoreRequest.php
@@ -28,7 +28,20 @@ class TeamStoreRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => 'required|team_name|unique:teams|max:20',
+            'display' => 'required|max:20',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required'     => 'Team IDを入力してください',
+            'name.team_name'    => 'Team IDは半角英数字またはハイフンのみで入力してください',
+            'name.unique'       => '入力されたTeam IDは既に使用されています',
+            'name.max'          => 'Team IDは20文字以内で入力してください',
+            'display.required'  => 'チーム名を入力してください',
+            'display.max' => 'チーム名は20文字以内で入力してください',
         ];
     }
 }
