5f79e9c6d88ba4da30485eb5368575071ab16d43
uenoryo <r.r.ag0611@outlook.jp>
2016-11-27 22:10:41 +0900

    Add Seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
+        $this->call(TeamsTableSeeder::class);
+        $this->call(TeamsUsersTableSeeder::class);
+        $this->call(MembersTableSeeder::class);
     }
 
     public static function truncateTable($table)
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -20,6 +20,18 @@ class UsersTableSeeder extends Seeder
                 'email'    => 'ourz@ourz.com',
                 'password' => bcrypt('ourz'),
             ],
+            [
+                'id'       => 2,
+                'name'     => 'ourz2',
+                'email'    => 'ourz2@ourz.com',
+                'password' => bcrypt('ourz2'),
+            ],
+            [
+                'id'       => 3,
+                'name'     => 'ourz3',
+                'email'    => 'ourz3@ourz.com',
+                'password' => bcrypt('ourz3'),
+            ],
         ];
 
         foreach ($users as $user) {
