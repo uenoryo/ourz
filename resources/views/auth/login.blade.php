274ba4ce2d39c8dbad72a67585451c83e3c3ffa0
KNJ <knj@wazly.net>
2016-11-30 02:02:38 +0900

    Refine log-in form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,55 +5,26 @@
   <div class="row">
     <div class="col-md-8 col-md-offset-2">
       <div class="panel panel-default">
-        <div class="panel-heading">Login</div>
+        <div class="panel-heading">ログイン</div>
         <div class="panel-body">
           <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
             {{ csrf_field() }}
-
-            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-              <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-              <div class="col-md-6">
-                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
-                @if ($errors->has('email'))
-                  <span class="help-block">
-                    <strong>{{ $errors->first('email') }}</strong>
-                  </span>
-                @endif
-              </div>
-            </div>
-
-            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-              <label for="password" class="col-md-4 control-label">Password</label>
-
-              <div class="col-md-6">
-                <input id="password" type="password" class="form-control" name="password" required>
-
-                @if ($errors->has('password'))
-                  <span class="help-block">
-                    <strong>{{ $errors->first('password') }}</strong>
-                  </span>
-                @endif
-              </div>
-            </div>
-
-            <div class="form-group">
-              <div class="col-md-6 col-md-offset-4">
-                <div class="checkbox">
-                  <label>
-                    <input type="checkbox" name="remember"> Remember Me
-                  </label>
-                </div>
-              </div>
-            </div>
-
-            <div class="form-group">
-              <div class="col-md-8 col-md-offset-4">
-                <md-button type="submit" class="md-raised md-primary">ログイン</md-button>
-                <md-button href="{{ url('/password/reset') }}" class="md-accent">パスワードを忘れましたか？</md-button>
-              </div>
-            </div>
+            <md-input-container class="{{ $errors->has('email') ? 'md-input-invalid' : '' }}">
+              <label>メールアドレス</label>
+              <md-input type="email" name="email" value="{{ old('email') }}" required autocomplete></md-input>
+              @if ($errors->has('email'))
+                <span class="md-error">{{ $errors->first('email') }}</span>
+              @endif
+            </md-input-container>
+            <md-input-container class="{{ $errors->has('password') ? 'md-input-invalid' : '' }}">
+              <label>パスワード</label>
+              <md-input type="password" name="password" required autocomplete></md-input>
+              @if ($errors->has('password'))
+                <span class="md-error">{{ $errors->first('password') }}</span>
+              @endif
+            </md-input-container>
+            <md-button type="submit" class="md-raised md-primary">ログイン</md-button>
+            <md-button href="{{ url('/password/reset') }}" class="md-accent">パスワードを忘れましたか？</md-button>
           </form>
         </div>
       </div>
